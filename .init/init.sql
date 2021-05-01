b3511d7d3494d52ea2db6961b8d9cc135169ca5e
Goryudyuma <6211370+Goryudyuma@users.noreply.github.com>
2021-05-01 13:10:48 +0900

    index追加

--- .init/init.sql
+++ .init/init.sql
@@ -38,5 +38,6 @@ CREATE TABLE `histories` (
   `user_id` int(11) NOT NULL,
   `created_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `IX_histories_product_id_user_id` (`product_id`,`user_id`)
+  KEY `IX_histories_product_id_user_id` (`product_id`,`user_id`),
+  KEY `IX_histories_user_id_id_product_id_created_at` (`user_id`,`id`,`product_id`,`created_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
